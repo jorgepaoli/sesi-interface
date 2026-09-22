eb6b6ad7cb8e41fbcd79c7d95ef8c9af47565cdc
Jorge <jorge.paoli@aluno.senai.br>
2026-09-22 14:33:16 -0300

    Terceiro versionamento (Implementação do Banco de Dados)

---
+++ app/api/alunos/route.js
@@ -0,0 +1,58 @@
+import db from "../../db/database";
+import { NextResponse } from "next/server";
+
+export async function listarAlunos(request){
+    const alunos = db.prepare('SELECT * FROM alunos ORDER BY nome');
+    return NextResponse.json(alunos);
+}
+
+export async function salvarAlunos(request){
+    try {
+        const dados = await request.json();
+        const sql = db.prepare(`INSERT INTO alunos (nome, idade, serie, ra) VALUES (?,?,?,?)`);
+        sql.run(
+            dados.nome,
+            dados.idade,
+            dados.serie,
+            dados.ra
+        );
+        return NextResponse.json({
+            mensagem: 'Aluno salvo com sucesso!'
+        });
+    } catch(error) {
+        console.log('Erro ao salvar aluno: ', error)
+    }
+}
+
+export async function editarAlunos(request){
+    try {
+        const dados = await request.json();
+        const sql = db.prepare(`UPDATE alunos (nome, idade, serie, ra) VALUES (?,?,?,?)`);
+        sql.run(
+            dados.nome,
+            dados.idade,
+            dados.serie,
+            dados.ra
+        );
+        return NextResponse.json({
+            mensagem: 'Aluno editado com sucesso!'
+        });
+    } catch(error) {
+        console.log('Erro ao editar aluno: ', error)
+    }
+}
+
+export async function excluirAlunos(request){
+    try {
+        const dados = await request.json();
+        const sql = db.prepare(`DELETE FROM alunos (id_aluno) VALUES (?)`);
+        sql.run(
+            dados.id_aluno
+        );
+        return NextResponse.json({
+            mensagem: 'Aluno excluído com sucesso!'
+        });
+    } catch(error) {
+        console.log('Erro ao excluir aluno: ', error)
+    }
+}
